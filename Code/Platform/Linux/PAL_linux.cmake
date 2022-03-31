fe1200751757a162990046bc11685c0f93872f6c
Valentin Bas <vbas@popcornfx.com>
2022-03-31 18:43:03 +0200

    PopcornFX Gem 2.12.0

--- Code/Platform/Linux/PAL_linux.cmake
+++ Code/Platform/Linux/PAL_linux.cmake
@@ -4,5 +4,5 @@
 #----------------------------------------------------------------------------
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
-ly_associate_package(PACKAGE_NAME PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux TARGETS PopcornFX PACKAGE_HASH c52b80c030aa7947ebb1954e7850f104b7a3dcd72ed34a256347be22381123f2)
+ly_associate_package(PACKAGE_NAME PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux TARGETS PopcornFX PACKAGE_HASH 3a4b7b1b9ef18018314810f363cc082d37650ed13c277827bc3496eda7255569)
 set(PAL_TRAIT_POPCORNFX_SUPPORTED TRUE)
--- Code/Platform/Mac/PAL_mac.cmake
+++ Code/Platform/Mac/PAL_mac.cmake
@@ -4,5 +4,5 @@
 #----------------------------------------------------------------------------
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
-ly_associate_package(PACKAGE_NAME PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-mac TARGETS PopcornFX PACKAGE_HASH 5de86161c5876cfbb5918fa83ade50fafe11d23b694ce46be85dc7c17d9f4ce6)
+ly_associate_package(PACKAGE_NAME PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-mac TARGETS PopcornFX PACKAGE_HASH 1ae660966858611d1effe525a4a3f2a9a5d2b77ecac174a5557c0bd14335463a)
 set(PAL_TRAIT_POPCORNFX_SUPPORTED TRUE)
