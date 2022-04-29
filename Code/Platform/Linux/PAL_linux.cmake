0d0bfde76ffb302aee327150c6ce7c7196d5b7a1
Valentin Bas <vbas@popcornfx.com>
2022-04-29 11:28:18 +0200

    PopcornFX Gem 2.12.2

--- Code/Platform/Linux/PAL_linux.cmake
+++ Code/Platform/Linux/PAL_linux.cmake
@@ -4,5 +4,5 @@
 #----------------------------------------------------------------------------
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
-ly_associate_package(PACKAGE_NAME PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux TARGETS PopcornFX PACKAGE_HASH 241e85588661a9ee5d336a1850da370db89953c0c75b1d911bd3316511b12ab8)
+ly_associate_package(PACKAGE_NAME PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux TARGETS PopcornFX PACKAGE_HASH a2610e6d5c188200817818c95284be24a02240237fca51749f17e22b75b78d9d)
 set(PAL_TRAIT_POPCORNFX_SUPPORTED TRUE)
