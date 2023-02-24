6d483fd8a9915d1c2dc530a892c4e7985b3220ad
Valentin Bas <vbas@popcornfx.com>
2023-02-24 10:46:52 +0100

    PopcornFX Gem 2.15.3

--- Code/Platform/Mac/PAL_mac.cmake
+++ Code/Platform/Mac/PAL_mac.cmake
@@ -5,8 +5,8 @@
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
 set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-mac)
-set(pk_package_hash b2b5ac75bb0357bb245fd60e54a7cd9a5b7ac65d9d4ef56e06aaaa82231ccbfd)
-set(pk_package_id wkYRZm3NjraDHkrJ)
+set(pk_package_hash 3b86f2a24940622897a51bc54875a0cc88f4d653ac6738357fba9d98b71b0c35)
+set(pk_package_id Dx8Bumnv6JMPHTyx)
 
 ly_associate_package(PACKAGE_NAME ${package_name} TARGETS PopcornFX PACKAGE_HASH ${pk_package_hash})
 pk_download_package_ifn(${package_name} ${pk_package_id})
