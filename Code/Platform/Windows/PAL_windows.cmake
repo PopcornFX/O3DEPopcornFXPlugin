ba2374ede337179a4823ca07c0332276532c7536
Valentin Bas <vbas@popcornfx.com>
2023-03-31 10:19:47 +0200

    PopcornFX Gem 2.15.5

--- Code/Platform/Windows/PAL_windows.cmake
+++ Code/Platform/Windows/PAL_windows.cmake
@@ -5,8 +5,8 @@
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
 set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-windows)
-set(pk_package_hash 2c33dc945512cb1a3f8bce22e140d6747ba90e0ee0d7a2674ae13af01797b407)
-set(pk_package_id hBPWpXze4nNvB36C)
+set(pk_package_hash b025157ba6389c6b5c3de7c89c0db406f73981caa5c6927afb072ee2b93f7215)
+set(pk_package_id ypyeht6XQndRnPJ7)
 
 ly_associate_package(PACKAGE_NAME ${package_name} TARGETS PopcornFX PACKAGE_HASH ${pk_package_hash})
 pk_download_package_ifn(${package_name} ${pk_package_id})
