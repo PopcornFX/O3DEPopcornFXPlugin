8d9f2910a36f0a80de3e2e6cb954a401a1691b64
Arthur Comte <acomte@popcornfx.com>
2023-06-01 17:46:14 +0200

    PopcornFX Gem 2.16.4

--- Code/Platform/Linux/PAL_linux.cmake
+++ Code/Platform/Linux/PAL_linux.cmake
@@ -5,8 +5,8 @@
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
 set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux)
-set(pk_package_hash 7eb7d96f248cef792af85d8f4c9f94dbb7445bf1fc5157735fd333772339ee06)
-set(pk_package_id ZgGPdBAbdP2Ab7AW)
+set(pk_package_hash 9a7a5231c729a5a0f5c5da8c5d5dde7b06eba56ab6e4ebc912142e07c27bb5c1)
+set(pk_package_id 2DK8uHtqfutDn3eH)
 
 ly_associate_package(PACKAGE_NAME ${package_name} TARGETS PopcornFX PACKAGE_HASH ${pk_package_hash})
 pk_download_package_ifn(${package_name} ${pk_package_id})
--- Code/Platform/Windows/PAL_windows.cmake
+++ Code/Platform/Windows/PAL_windows.cmake
@@ -5,8 +5,8 @@
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
 set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-windows)
-set(pk_package_hash e041bea0b04dc43febe1ce3ad6659ffd4b042821e1f64499117e267cc44dd240)
-set(pk_package_id cNPcAwcwCdrVWKup)
+set(pk_package_hash 6e342027f04be3bf4a9724ef12acff2586231de13c8568b191ea6dadbf8c9c5b)
+set(pk_package_id rduPRStDNeGSNnLL)
 
 ly_associate_package(PACKAGE_NAME ${package_name} TARGETS PopcornFX PACKAGE_HASH ${pk_package_hash})
 pk_download_package_ifn(${package_name} ${pk_package_id})
