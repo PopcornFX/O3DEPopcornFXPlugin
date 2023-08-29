0181bebf20835101913b6cfd663762f4d8c19b7e
Valentin Bas <vbas@persistant.fr>
2023-08-29 17:38:38 +0200

    Add linux aarch64 package hash

--- Code/Platform/Linux/PAL_linux.cmake
+++ Code/Platform/Linux/PAL_linux.cmake
@@ -5,8 +5,15 @@
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
 set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux)
-set(pk_package_hash 0624bf8ff38bce909fbedede6647990d527c8f11ada08000cdd535fc8c2ee121)
-set(pk_package_id 2pEhBPX228JQwwAQ)
+if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+	set(pk_package_hash 0624bf8ff38bce909fbedede6647990d527c8f11ada08000cdd535fc8c2ee121)
+	set(pk_package_id 2pEhBPX228JQwwAQ)
+elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
+	set(pk_package_hash 0017707d58f80cac493048386a6d703ccd498515633430da9c63a4ad5a1ed564)
+	set(pk_package_id POPCORNFXPACKAGEIDTOREPLACE_ARM64)
+else()
+	message(FATAL_ERROR "Unsupported linux architecture ${CMAKE_SYSTEM_PROCESSOR}")
+endif()
 
 ly_associate_package(PACKAGE_NAME ${package_name} TARGETS PopcornFX PACKAGE_HASH ${pk_package_hash})
 pk_download_package_ifn(${package_name} ${pk_package_id})
