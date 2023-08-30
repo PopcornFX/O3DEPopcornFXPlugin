41c2650f66302f61b40ac45f77cdf1786e8fed5a
Valentin Bas <vbas@persistant.fr>
2023-08-30 22:04:27 +0200

    Update linux aarch64 package

--- Code/Platform/Linux/PAL_linux.cmake
+++ Code/Platform/Linux/PAL_linux.cmake
@@ -4,12 +4,11 @@
 #----------------------------------------------------------------------------
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
+set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux)
 if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-	set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux)
-	set(pk_package_hash 0624bf8ff38bce909fbedede6647990d527c8f11ada08000cdd535fc8c2ee121)
-	set(pk_package_id 2pEhBPX228JQwwAQ)
+	set(pk_package_hash POPCORNFXPACKAGEHASHTOREPLACE)
+	set(pk_package_id POPCORNFXPACKAGEIDTOREPLACE)
 elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
-	set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-linux-aarch64)
 	set(pk_package_hash 0017707d58f80cac493048386a6d703ccd498515633430da9c63a4ad5a1ed564)
 	set(pk_package_id POPCORNFXPACKAGEIDTOREPLACE_ARM64)
 else()
