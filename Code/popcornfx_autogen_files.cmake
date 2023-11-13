7621d3917c5c18b374737212ed547562c0b890dc
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2023-11-13 14:15:35 -0800

    Support ScriptCanvas 2.0.0 changes
    Related PR https://github.com/o3de/o3de/pull/16678 Script Canvas node registration simplification
    
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- Code/popcornfx_autogen_files.cmake
+++ Code/popcornfx_autogen_files.cmake
@@ -5,10 +5,23 @@
 
 get_property(scriptcanvas_gem_root GLOBAL PROPERTY "@GEMROOT:ScriptCanvas@")
 
+set(PK_OPTIONAL_REGISTER_NODEABLE_FILES 
+    ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeableRegistry_Header.jinja
+    ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeableRegistry_Source.jinja
+    )
+
+if(DEFINED O3DE_VERSION_MAJOR)
+    # O3DE 4.x.x and higher no longer need the registry source files
+    # Also include special case to handle bug where SDK 23.05.0 engine version was set to the display version
+    if(O3DE_VERSION_MAJOR GREATER_EQUAL 4 AND 
+        NOT (O3DE_VERSION_MAJOR EQUAL 23 AND O3DE_VERSION_MINOR EQUAL 05 AND O3DE_VERSION_PATCH EQUAL 0))
+        unset(PK_OPTIONAL_REGISTER_NODEABLE_FILES)
+    endif()
+endif()
+
 set(FILES
     ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeable_Header.jinja
     ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeable_Source.jinja
-    ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeableRegistry_Header.jinja
-    ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeableRegistry_Source.jinja
+    ${PK_OPTIONAL_REGISTER_NODEABLE_FILES}
 )
 
