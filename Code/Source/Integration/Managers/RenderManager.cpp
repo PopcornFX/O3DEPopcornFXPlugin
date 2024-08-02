8056c511f68b4c23a9674cbe6c07fc449364b235
Valentin Bas <vbas@persistant.fr>
2024-08-02 14:32:12 +0200

    Fixed  O3DE Development Branch Compilation error #77

--- Code/Source/Integration/Managers/RenderManager.cpp
+++ Code/Source/Integration/Managers/RenderManager.cpp
@@ -52,10 +52,11 @@ void	CRenderManager::Activate(CParticleMediumCollection *mediumCollection, const
 	dynamicPoolDescriptor.m_hostMemoryAccess = AZ::RHI::HostMemoryAccess::Write;
 	dynamicPoolDescriptor.m_bindFlags = AZ::RHI::BufferBindFlags::InputAssembly | AZ::RHI::BufferBindFlags::ShaderRead;
 	dynamicPoolDescriptor.m_largestPooledAllocationSizeInBytes = 0x100000;
+	dynamicPoolDescriptor.m_deviceMask = AZ::RHI::MultiDevice::DefaultDevice;
 
 #if O3DE_VERSION_MAJOR >= 4 && O3DE_VERSION_MINOR >= 2
 	m_BufferPool = aznew AZ::RHI::BufferPool;
-	AZ::RHI::ResultCode resultCode = m_BufferPool->Init(AZ::RHI::MultiDevice::DefaultDevice, dynamicPoolDescriptor);
+	AZ::RHI::ResultCode resultCode = m_BufferPool->Init(dynamicPoolDescriptor);
 #else
 	m_BufferPool = AZ::RHI::Factory::Get().CreateBufferPool();
 	AZ::RHI::ResultCode resultCode = m_BufferPool->Init(*AZ::RHI::RHISystemInterface::Get()->GetDevice(), dynamicPoolDescriptor);
