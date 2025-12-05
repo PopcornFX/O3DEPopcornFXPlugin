415d38c73902e491cfea2e29d557233860ee72a7
Valentin Bas <vbas@persistant.fr>
2025-12-05 12:40:59 +0100

    Revert "PopcornFX Gem 2.22.6"
    This reverts commit 9f68f73ea1ad2f7df1ab94356bca9e4bb2adba91.

--- Code/Platform/Windows/PAL_windows.cmake
+++ Code/Platform/Windows/PAL_windows.cmake
@@ -5,8 +5,8 @@
 
 set(LY_PACKAGE_SERVER_URLS ${LY_PACKAGE_SERVER_URLS} "https://downloads.popcornfx.com/o3de-packages")
 set(package_name PopcornFX-${POPCORNFX_VERSION}-${POPCORNFX_LICENSE}-windows)
-set(pk_package_hash 0cf2a7911c2434d4996ea63dfe84a3c710444437d6f33319aaf8fcca6878a482)
-set(pk_package_id a37wYWSMD3YgiqrP)
+set(pk_package_hash a7460fce42c5d88bc3c37cfb8f676e4995cb99a1b2489efe40b6b9f6444cf4f1)
+set(pk_package_id hToBb5AqKI5HipJq)
 
 ly_associate_package(PACKAGE_NAME ${package_name} TARGETS PopcornFX PACKAGE_HASH ${pk_package_hash})
 pk_download_package_ifn(${package_name} ${pk_package_id})
